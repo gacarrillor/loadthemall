525579ac2f3de1792c55c8faf9ba5ba988849cd8
Germán Carrillo <gcarrillo@linuxmail.org>
2023-01-23 10:46:19 +0100

    Add support for .kap files (fix #35)

--- core/Filter.py
+++ core/Filter.py
@@ -1,3 +1,23 @@
+"""
+/***************************************************************************
+LoadThemAll
+A QGIS plugin
+Loads files stored in a directory structure recursively, based on several filters
+                             -------------------
+begin                : 2010-10-03
+copyright            : (C) 2010 by Germán Carrillo (GeoTux)
+email                : gcarrillo@linuxmail.org
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
 import os, re
 import time
 from abc import (ABC,
--- gui/BaseLoadThemAllDialog.py
+++ gui/BaseLoadThemAllDialog.py
@@ -1,9 +1,30 @@
+"""
+/***************************************************************************
+LoadThemAll
+A QGIS plugin
+Loads files stored in a directory structure recursively, based on several filters
+                             -------------------
+begin                : 2010-10-03
+copyright            : (C) 2010 by Germán Carrillo (GeoTux)
+email                : gcarrillo@linuxmail.org
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
 from qgis.PyQt.QtCore import (Qt,
                               QSettings)
 from qgis.PyQt.QtWidgets import (QApplication,
                                  QDialog,
                                  QFileDialog)
 
+from core.FileFormatConfiguration import VECTOR_FORMATS, RASTER_FORMATS
 from ..ui.Ui_Base_LoadThemAll import Ui_Base_LoadThemAll
 
 
@@ -42,55 +63,7 @@ class BaseLoadThemAllDialog(QDialog, Ui_Base_LoadThemAll):
 
     def loadFormats(self, isVector):
         """ Fill the comboBox with file formats """
-        if isVector:
-            allFormats = [
-                ("GeoPackage (*.gpkg)", [".gpkg"]),
-                ("ESRI Shapefile (*.shp)", [".shp"]),
-                ("Mapinfo File (*.mif, *.tab)", [".mif", ".tab"]),
-                ("Microstation DGN (*.dgn)", [".dgn"]),
-                ("VRT - Virtual Datasource (*.vrt)", [".vrt"]),
-                ("Comma Separated Value (*.csv)", [".csv"]),
-                ("Geography Markup Language (*.gml)", [".gml"]),
-                ("GPX (*.gpx)", [".gpx"]),
-                ("KML - Keyhole Markup Language (*.kml, *.kmz)", [".kml", ".kmz"]),
-                ("GeoJSON (*.geojson)", [".geojson"]),
-                ("GMT (*.gmt)", [".gmt"]),
-                ("SQLite (*.sqlite)", [".sqlite"]),
-                ("Arc/Info ASCII Coverage (*.e00)", [".e00"]),
-                ("AutoCAD DXF (*.dxf)", [".dxf"]),
-                ("JSON (*.json)", [".json"]),
-            ]
-        else:
-            allFormats = [
-                ("Virtual Raster (*.vrt)", [".vrt"]),
-                ("GeoTIFF (*.tif, *.tiff)", [".tif", ".tiff"]),
-                ("Erdas Imagine Images (*.img)", [".img"]),
-                ("Erdas Compressed Wavelets (*.ecw)", [".ecw"]),
-                ("ERMapper .ers Labelled (*.ers)", [".ers"]),
-                ("DTED Elevation Raster (*.dt)", [".dt2", ".dt3"]),
-                ("Arc/Info ASCII Grid (*.asc)", [".asc"]),
-                ("Portable Network Graphics (*.png)", [".png"]),
-                ("JPEG JFIF (*.jpg, *.jpeg)", [".jpg", ".jpeg"]),
-                ("JPEG2000 (*.jp2)", [".jp2"]),
-                ("Graphics Interchange Format (*.gif)", [".gif"]),
-                ("X11 PixMap Format (*.xpm)", [".xpm"]),
-                ("Bitmap image file (*.bmp)", [".bmp"]),
-                ("PCIDSK Database File (*.pix)", [".pix"]),
-                ("PCRaster Raster File (*.map)", [".map"]),
-                ("ILWIS Raster Map (*.mpr, *.mpl)", [".mpr", ".mpl"]),
-                ("SRTMHGT File Format (*.hgt)", [".hgt"]),
-                ("GMT NetCDF Grid Format (*.nc)", [".nc"]),
-                ("GRIdded Binary (*.grb)", [".grb"]),
-                ("Idrisi Raster A.1 (*.rst)", [".rst"]),
-                ("Golden Software ASCII Grid (*.grd)", [".grd"]),
-                ("R Object Data Store (*.rda)", [".rda"]),
-                ("Vexcel MFF Raster (*.hdr)", [".hdr"]),
-                ("USGS Optional ASCII DEM (*.dem)", [".dem"]),
-                ("Magellan topo (*.blx)", [".blx"]),
-                ("Rasterlite (*.sqlite)", [".sqlite"]),
-                ("SAGA GIS Binary Grid (*.sdat)", [".sdat"]),
-                ("Multi-resolution Seamless Image Database (*.sid)", [".sid"]),
-            ]
+        allFormats = VECTOR_FORMATS if isVector else  RASTER_FORMATS
         allExtensions = [extension for format in allFormats for extension in format[1]]
         self.cboFormats.addItem("All listed formats (*.*)", allExtensions)
         for format in allFormats:
