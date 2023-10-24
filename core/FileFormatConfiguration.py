ccb9ca53843a6991c66788bc7e3f0ad1cb45badd
Germán Carrillo <gcarrillo@linuxmail.org>
2023-10-24 19:25:29 +0200

    Support searching for layers inside RAR files

--- core/FileFormatConfiguration.py
+++ core/FileFormatConfiguration.py
@@ -3,7 +3,7 @@
 #      All file extensions should be written in lower case.
 #      In the code we make sure to properly test against variations (e.g., uppercase).
 
-COMPRESSED_FILE_EXTENSIONS = [".zip"]
+COMPRESSED_FILE_EXTENSIONS = [".zip", ".rar"]
 
 VECTOR_FORMATS = [
     ("GeoPackage (*.gpkg)", [".gpkg"]),
