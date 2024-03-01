cf729fd0f7fe92f9827cf92a8ba8b8dd1b9bf887
Germán Carrillo <gcarrillo@linuxmail.org>
2024-03-01 12:20:40 +0100

    [tests] Properly test results of a Load Point Cloud session; new test 'not-found ept.json layers'

--- tests/test_load_point_clouds.py
+++ tests/test_load_point_clouds.py
@@ -6,6 +6,7 @@ from qgis.testing import unittest, start_app
 from qgis.testing.mocked import get_iface
 
 from LoadThemAll.LoadThemAll import LoadThemAll
+from LoadThemAll.core.Enums import EnumLoadThemAllResult
 from LoadThemAll.core.LoadFiles import LoadPointClouds
 from LoadThemAll.core.LoadConfiguration import LoadConfiguration
 
@@ -16,14 +17,15 @@ class TestLoadPointClouds(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        print('\nINFO: Set up test_plugin_load')
+        print('\nINFO: Set up test_load_point_clouds')
         cls.plugin = LoadThemAll(get_iface(), with_gui=False)
         cls.project = QgsProject.instance()
 
-    def test_load_point_clouds(self):
+    def test_load_point_clouds_single_ept(self):
         configuration = LoadConfiguration()
         configuration.base_dir = "/QGIS/tests/testdata/point_clouds/ept/rgb/"
         configuration.extension = ["ept.json"]
+        configuration.with_gui = False  # Non-default
         configuration.b_sort = False  # Non-default
 
         configuration.b_groups = False
@@ -41,7 +43,10 @@ class TestLoadPointClouds(unittest.TestCase):
         # Load 1 layer
         loader = LoadPointClouds(self.plugin.iface, configuration)
         self.assertEqual(self.project.count(), 0)
-        loader.loadLayers()
+        res = loader.loadLayers()
+        self.assertEqual(res.result, EnumLoadThemAllResult.SUCCESS)
+        self.assertEqual(res.layers_found, 1)
+        self.assertEqual(res.layers_loaded, 1)
         self.assertTrue(self.project.count() > 0)
 
         # Check loaded layer
@@ -49,10 +54,42 @@ class TestLoadPointClouds(unittest.TestCase):
         self.assertTrue(bool(layers))
         self.assertTrue(layers[0].isValid())
 
+    def test_load_point_clouds_ept_not_found(self):
+        configuration = LoadConfiguration()
+        configuration.base_dir = "/QGIS/tests/testdata/point_clouds/ept/rgb/ept-sources/"
+        configuration.extension = ["ept.json"]
+        configuration.with_gui = False  # Non-default
+        configuration.b_sort = False  # Non-default
+
+        configuration.b_groups = False
+        configuration.b_search_in_compressed_files = False
+        configuration.b_layers_off = False
+        configuration.b_not_empty = True
+        configuration.b_reverse_sort = False
+        configuration.b_case_insensitive = True
+        configuration.b_accent_insensitive = False
+        configuration.b_styles = False
+        configuration.b_search_parent_layer = False
+        configuration.b_add_parent_layer_name = True
+        configuration.num_layers_to_confirm = 50
+
+        # Load 1 layer
+        loader = LoadPointClouds(self.plugin.iface, configuration)
+        self.assertEqual(self.project.count(), 0)
+        res = loader.loadLayers()
+
+        self.assertEqual(res.result, EnumLoadThemAllResult.SUCCESS)
+        self.assertEqual(res.layers_found, 0)
+        self.assertEqual(res.layers_loaded, 0)
+        self.assertEqual(self.project.count(), 0)
+
+    def tearDown(self):
+        print('INFO: Removing all layers after load test...')
+        self.project.removeAllMapLayers()
+
     @classmethod
     def tearDownClass(cls):
-        print('INFO: Tear down test_plugin_load')
-        cls.project.removeAllMapLayers()
+        print('INFO: Tear down test_load_point_clouds')
         cls.plugin.unload()
 
 
