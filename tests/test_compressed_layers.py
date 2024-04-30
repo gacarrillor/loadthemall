d31de7a8e4997db259b71aecece12641d7773f8a
Germán Carrillo <gcarrillo@linuxmail.org>
2024-04-30 13:35:03 +0200

    [tests] Add test for compressed SHP files

--- tests/test_compressed_layers.py
+++ tests/test_compressed_layers.py
@@ -24,6 +24,46 @@ class TestCompressedLayers(unittest.TestCase):
         cls.plugin = LoadThemAll(get_iface(), with_gui=False)
         cls.project = QgsProject.instance()
 
+    def test_compressed_shp(self):
+        configuration = LoadConfiguration()
+        configuration.base_dir = "/QGIS/tests/testdata/zip/"
+        configuration.extension = [".shp"]
+        configuration.with_gui = False  # Non-default
+        configuration.b_sort = False  # Non-default
+
+        configuration.b_groups = False
+        configuration.b_search_in_compressed_files = True
+        configuration.b_layers_off = False
+        configuration.b_not_empty = True
+        configuration.b_reverse_sort = False
+        configuration.b_case_insensitive = True
+        configuration.b_accent_insensitive = False
+        configuration.b_styles = False
+        configuration.b_search_parent_layer = False
+        configuration.b_add_parent_layer_name = False
+        configuration.num_layers_to_confirm = 50
+
+        # Set filters for next session
+        filter_list = FilterList()
+        filter = AlphanumericFilter('EndsWith', 'points', configuration)
+        filter_list.addFilter(filter)
+
+        # Load 1 layer
+        loader = LoadVectors(self.plugin.iface, configuration)
+        self.assertEqual(self.project.count(), 0)
+        loader.filterList = filter_list
+        res = loader.loadLayers()
+        self.assertEqual(res.result, EnumLoadThemAllResult.SUCCESS)
+        self.assertEqual(res.layers_found, 4)
+        self.assertEqual(res.layers_loaded, 4)
+        self.assertTrue(self.project.count() > 0)
+
+        # Check loaded layer
+        #print([l.source() for l in self.project.mapLayers().values()])
+        layers = self.project.mapLayersByName("points")
+        self.assertEqual(len(layers), 4)
+        self.assertTrue(layers[0].isValid())
+
     def test_compressed_gz_geojson(self):
         configuration = LoadConfiguration()
         configuration.base_dir = "/QGIS/tests/testdata/zip/"
